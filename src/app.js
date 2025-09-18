46981135d192890b6d725f739621a4fbc77b3635
akhilesh091 <akhileshts091@gmail.com>
2025-09-18 15:07:13 +0530

    multipple route handlers

--- src/app.js
+++ src/app.js
@@ -2,24 +2,41 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user/:id/:name/:city/:job", (req, res) => {
+  console.log(req.params);
+  res.send({
+    First_Name: "akhilesh",
+    Last_Name: "Ts",
+    City: "Dubai",
+    Job: "Software Engineer",
+  });
+});
 
-app.get('/user/:id/:name/:city/:job',(req,res)=>{
-  // const {id,name}=req.query
-  // console.log(req.query)
-  // console.log(id,name)
-  console.log(req.params)
-  res.send({First_Name:"akhilesh",Last_Name:"Ts",City:"Dubai",Job:"Software Engineer"})
-})
+app.get(
+  "/user",
+  (req, res, next) => {
+    // res.send("no user found 1");
+    next()
+  },
+  (req, res,next) => {
+    // res.send("no user found 2");
+    next();
+  }
+  ,
+  (req, res,next) => {
+    // res.send("no user found 2");
+    next();
+  }
+  
+);
 
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved");
+});
 
-
-app.post("/user",(req,res)=>{
-  res.send("Data successfully saved")
-})
-
-app.delete('/user',(req,res)=>{
-  res.send("Deleted Sucessfully deleted")
-})
+app.delete("/user", (req, res) => {
+  res.send("Deleted Sucessfully deleted");
+});
 
 app.listen(7777, () => {
   console.log("Server running successfully on the server 7777");
