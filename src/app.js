140f92b6a0fadf62dc677cf80b9db8dfafe3bb79
akhilesh091 <akhileshts091@gmail.com>
2025-09-18 11:34:58 +0530

    playing with the route

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ const express = require("express");
 const app = express();
 
 
-app.get('/user',(req,res)=>{
+app.get('/user/:id/:name/:city/:job',(req,res)=>{
+  // const {id,name}=req.query
+  // console.log(req.query)
+  // console.log(id,name)
+  console.log(req.params)
   res.send({First_Name:"akhilesh",Last_Name:"Ts",City:"Dubai",Job:"Software Engineer"})
 })
+
+
 
 app.post("/user",(req,res)=>{
   res.send("Data successfully saved")
@@ -14,7 +20,6 @@ app.post("/user",(req,res)=>{
 app.delete('/user',(req,res)=>{
   res.send("Deleted Sucessfully deleted")
 })
-
 
 app.listen(7777, () => {
   console.log("Server running successfully on the server 7777");
